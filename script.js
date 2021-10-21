83957171bf85df318fe479f0603f60620247ca33
Edward Paler <paler.edward@gmail.com>
2021-10-21 11:18:07 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,56 @@
+// 3. Create a "trainer" object by using object literals.
+// 4. Initialize/add the "trainer" object properties.
+
+let trainer = {
+	name: "Ash Ketchum",
+	age: 10,
+	pokemon: ['Pikachu', 'Charizard', 'Squirtle', 'Bulbasaur'],
+	friends: {
+		hoenn: ['May', 'Max'],
+		kanto: ['Brock', 'Misty']
+	},
+	talk: function(pokemone){
+	let pokemonName = trainer.pokemon.forEach(function(pokemone){
+		return(trainer.pokemon == pokemone)
+	})
+	console.log(pokemonName);
+	console.log(pokemonName + "! I choose you!");
+		
+	}
+}
+
+// 5. Access the "trainer" object properties using dot and square bracket notation.
+console.log('Result of dot notation:')
+console.log(trainer.name);
+console.log('Result of bracket notation')
+
+// 6. Invoke/call the "trainer" object method.
+
+// 7. Create a constructor function for creating a pokemon.
+
+function Pokemon(name, level){
+	//Properties
+	this.name = name;
+	this.level = level;
+	this.health = 2 * level;
+	this.attack = level;
+
+	//Methods
+	this.tackle = function(target){
+		console.log(this.name + ' tackled ' + target.name);
+		healthLeft = target.health - this.attack;
+		console.log(target.name " health is now reduced to " + healthLeft);
+	};	
+	this.faint = function(){
+		console.log(this.name + " fainted.");
+	}
+}
+// 8. Create/instantiate several pokemon using the "constructor" function.
+let pikachu = new Pokemon("Pikachu", 16);
+let rattata = new Pokemon("Rattata", 8);
+let torchic = new Pokemon("Torchic", 10);
+let geodude = new Pokemon("Geodude", 10);
+
+// 9. Have the pokemon objects interact with each other by calling on the "tackle" method.
+console.log(Pokemon);
+pikachu.tackle(rattata);
